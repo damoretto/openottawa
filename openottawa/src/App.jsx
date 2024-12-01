d27cfa77ebca5aace4b40fa73e747e2833753bb5
Dane <dane.amoretto@uottawa.ca>
2024-12-01 17:02:17 -0500

    added scroll to top

--- openottawa/src/App.jsx
+++ openottawa/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home.jsx";
 import Somerset from "./Pages/Somerset/Somerset.jsx";
+import ScrollToTop from './ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/somerset" element={<Somerset />} />
