76a6e6cf348277550f042064f87bf840bc9fffce
Dane <dane.amoretto@uottawa.ca>
2024-11-30 23:39:05 -0500

    added clickable map

--- openottawa/src/Pages/Home/Home.jsx
+++ openottawa/src/Pages/Home/Home.jsx
@@ -1,18 +1,44 @@
-import React from "react";
-import "./Home.css";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
+  const [svgContent, setSvgContent] = useState(""); // State to hold the SVG content
   const navigate = useNavigate();
-  
+
+  useEffect(() => {
+    fetch("/wardmap.svg")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to load SVG");
+        }
+        return response.text();
+      })
+      .then((data) => setSvgContent(data))
+      .catch((error) => console.error("Error loading SVG:", error));
+  }, []);
+
+  const handleRegionClick = (e) => {
+    const regionId = e.target.id;
+    switch (regionId) {
+      case "somerset":
+        navigate("/somerset");
+        break;
+      default:
+        console.log(`Clicked on an unrecognized region: ${regionId}`);
+    }
+  };
+
   return (
     <div className="home-container">
+      <h1>Select a Ward</h1>
+      <p>Explore the map below. Click on a ward to learn more about it.</p>
 
-      <h1>Ward Selection</h1>
-      <button className="button" onClick={() => navigate("/somerset")}>
-        Somerset
-      </button>
-
+      <div
+        dangerouslySetInnerHTML={{ __html: svgContent }}
+        onClick={handleRegionClick}
+        className={"svg-container"}
+      />
     </div>
   );
 }
